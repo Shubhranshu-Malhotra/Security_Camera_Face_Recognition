c4cc3fafb4c1ef1b758388fe68592bc7c52ede5a
Shubhranshu-Malhotra <shubhranshumalhotra.sm@gmail.com>
2021-07-11 20:22:05 +0530

    testing speed of face detection

--- simple_face_detection.py
+++ simple_face_detection.py
@@ -1,11 +1,12 @@
 import cv2
 import wget
+import time
 print("imports successful!!")
 
 # download the cascade
 url = "https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml"
 
-wget.download(url)
+# wget.download(url)
 
 # Load cascade
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
@@ -21,6 +22,9 @@ print('Is the IP camera turned on: {}'.format(cam.isOpened()))
 
 while True:
     
+    # Time to process one frame
+
+    start = time.time()
     # Read the frame
     _, frame = cam.read()
 
@@ -34,6 +38,13 @@ while True:
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
+    # End time
+    end = time.time()
+    time_taken = end - start
+    fps = 1/time_taken
+    cv2.rectangle(frame, (0, 0), (80, 50), (0, 0, 255), 3)
+    font=cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(frame, f"{fps:.1f}", (20,20),fontFace = font, fontScale = 0.5, color = (0,0,255))
     # Display
     cv2.imshow('Face Detection', frame)
     cv2.moveWindow('Face Detection',0,0)
