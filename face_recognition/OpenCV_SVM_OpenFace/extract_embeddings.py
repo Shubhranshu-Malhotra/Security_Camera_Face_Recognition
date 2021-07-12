15d8813665c31f2666c2b79b9af6c7ede2d9f5f5
Shubhranshu-Malhotra <shubhranshumalhotra.sm@gmail.com>
2021-07-13 01:14:14 +0530

    extract and save embeddings

--- face_recognition/OpenCV_SVM_OpenFace/extract_embeddings.py
+++ face_recognition/OpenCV_SVM_OpenFace/extract_embeddings.py
@@ -64,6 +64,7 @@ for (i, imagePath) in enumerate(imagePaths):
 	image = cv2.imread(imagePath)
 	image = imutils.resize(image, width=600)
 	(h, w) = image.shape[:2]
+
 	# construct a blob from the image
 	imageBlob = cv2.dnn.blobFromImage(
 		cv2.resize(image, (300, 300)), 1.0, (300, 300),
@@ -72,3 +73,51 @@ for (i, imagePath) in enumerate(imagePaths):
 	# faces in the input image
 	detector.setInput(imageBlob)
 	detections = detector.forward()
+	# ensure at least one face was found
+	if len(detections) > 0:
+		# we're making the assumption that each image has only ONE
+		# face, so find the bounding box with the largest probability
+		i = np.argmax(detections[0, 0, :, 2])
+		confidence = detections[0, 0, i, 2]
+		# ensure that the detection with the largest probability also
+		# means our minimum probability test (thus helping filter out
+		# weak detections)
+		if confidence > args["confidence"]:
+			# compute the (x, y)-coordinates of the bounding box for
+			# the face
+			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+			(startX, startY, endX, endY) = box.astype("int")
+			# extract the face ROI and grab the ROI dimensions
+			face = image[startY:endY, startX:endX]
+			(fH, fW) = face.shape[:2]
+			# ensure the face width and height are sufficiently large
+			if fW < 20 or fH < 20:
+				continue
+
+			# construct a blob for the face ROI, then pass the blob
+			# through our face embedding model to obtain the 128-d
+			# quantification of the face
+			faceBlob = cv2.dnn.blobFromImage(face, 1.0 / 255,
+				(96, 96), (0, 0, 0), swapRB=True, crop=False)
+			embedder.setInput(faceBlob)
+			vec = embedder.forward()
+			# add the name of the person + corresponding face
+			# embedding to their respective lists
+			knownNames.append(name)
+			knownEmbeddings.append(vec.flatten())
+			total += 1
+	print("[INFO] processed image {}/{}".format(total, len(imagePaths)))
+
+# dump the facial embeddings + names to disk
+print("[INFO] serializing {} encodings...".format(total))
+data = {"embeddings": knownEmbeddings, "names": knownNames}
+f = open(args["embeddings"], "wb")
+f.write(pickle.dumps(data))
+f.close()
+
+
+## To extract face embeddings run the following command in cmd:
+# python extract_embeddings.py --dataset dataset --embeddings output/embeddings.pickle --detector face_detection_model --embedding-model nn4.small2.v1.t7 
+
+
+	
